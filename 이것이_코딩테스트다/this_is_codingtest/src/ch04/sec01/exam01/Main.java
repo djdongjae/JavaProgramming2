f1814bb11e02f2f1c6858822406b9e92e2fa568b
djdongjae <jk47wee@gmail.com>
2023-09-06 10:41:38 +0900

    문제 정보 추가

--- 이것이_코딩테스트다/this_is_codingtest/src/ch04/sec01/exam01/Main.java
+++ 이것이_코딩테스트다/this_is_codingtest/src/ch04/sec01/exam01/Main.java
@@ -1,23 +1,37 @@
 package ch04.sec01.exam01;
 
 import java.util.*;
-
-public class Main{
+// ch04 상하좌우(구현)
+public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         sc.nextLine();
         String[] plans = sc.nextLine().split(" ");
 
-        int x = 1, y = 1;
+        int x = 1;
+        int y = 1;
 
         int[] dx = {0, 0, -1, 1};
         int[] dy = {-1, 1, 0, 0};
         char[] moveTypes = {'L', 'R', 'U', 'D'};
 
         for (int i = 0; i < plans.length; i++) {
-
+            int nx = -1;
+            int ny = -1;
+            char plan = plans[i].charAt(0);
+            for (int j = 0; j < 4; j++) {
+                if (plan == moveTypes[j]) {
+                    nx = x + dx[j];
+                    ny = y + dy[j];
+                }
+            }
+            if (nx < 1 || ny < 1 || nx > n || ny > n) continue;
+            x = nx;
+            y = ny;
         }
+
+        System.out.println(x + " " + y);
     }
 }
