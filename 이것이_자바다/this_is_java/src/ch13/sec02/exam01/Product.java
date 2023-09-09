16812002b5aa0ad016bafd1206df4da80ff92776
djdongjae <jk47wee@gmail.com>
2023-09-09 14:14:24 +0900

    제네릭

--- 이것이_자바다/this_is_java/src/ch13/sec02/exam01/Product.java
+++ 이것이_자바다/this_is_java/src/ch13/sec02/exam01/Product.java
@@ -5,4 +5,19 @@ public class Product<K, M> {
     private K kind; // 제네릭 타입
     private M model; // 제네릭 모델 타입
 
+    public K getKind() {
+        return kind;
+    }
+
+    public void setKind(K kind) {
+        this.kind = kind;
+    }
+
+    public M getModel() {
+        return model;
+    }
+
+    public void setModel(M model) {
+        this.model = model;
+    }
 }
