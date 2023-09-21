80180256e74ae5495a4a239b150274e801274e76
djdongjae <jk47wee@gmail.com>
2023-09-21 21:35:38 +0900

    알고리즘 merge 과제 주석 추가

--- 알고리즘/algorithm/src/lab4_1/Main_201914089.java
+++ 알고리즘/algorithm/src/lab4_1/Main_201914089.java
@@ -39,36 +39,37 @@ public class Main_201914089 {
 
     public static void merge(int[] array, int lb, int m, int ub) {
         int[] tmp = new int[ub - lb + 1]; // 결과를 임시 저장할 tmp 배열 생성
-        int left = lb; //
-        int right = m + 1;
-        int index = left;
+        int left = lb; // 왼쪽 배열의 시작점
+        int right = m + 1; // 오른쪽 배열의 시작점
+        int index = left; // tmp 배열의 시작점
 
-        while (left <= m && right <=ub) {
-            if (array[left] < array[right]) {
-                tmp[index] = array[left];
-                left++;
+        while (left <= m && right <=ub) { // 왼쪽 배열과 오른쪽 배열의 인덱스가 범위를 벗어나지 않는 동안
+            if (array[left] < array[right]) { // 왼쪽 값과 오른쪽 값을 비교하며
+                tmp[index] = array[left]; // 왼쪽이 작을 경우 먼저 저장
+                left++; // 왼쪽 인덱스 1 증가
             } else {
-                tmp[index] = array[right];
-                right++;
+                tmp[index] = array[right]; // 오른쪽이 작을 경우 먼저 저장
+                right++; // 오른쪽 인덱스 1 증가
             }
-            index++;
+            index++; // 임시 배열 인데스 증가
         }
 
-        if (left <= m && right > ub) {
-            while (left <= m) {
-                tmp[index] = array[left];
+        // 왼쪽 오른쪽 중 아직 다 순회하지 못한 배열에 대해 처리
+        if (left <= m && right > ub) { // 왼쪽 배열이 아직 전부 처리되지 못했을 경우
+            while (left <= m) { // 남은 왼쪽 배열을 순회하며
+                tmp[index] = array[left]; // 임시 배열에 저장
                 left++;
                 index++;
             }
-        } else if(left > m && right <= ub) {
-            while (right <= ub) {
-                tmp[index] = array[right];
+        } else if(left > m && right <= ub) { // 오른쪽 배열이 아직 전부 처리되지 못했을 경우
+            while (right <= ub) { // 남은 오른쪽 배열을 순회하며
+                tmp[index] = array[right]; // 임시 배열에 저장
                 right++;
                 index++;
             }
         }
 
-        for (int i = lb; i <= ub; i++) {
+        for (int i = lb; i <= ub; i++) { // 임시 배열을 기존 배열로 복사
             array[i] = tmp[i];
         }
     }
