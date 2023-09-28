d69edc2825aea941759556ababb9fef907073d9f
djdongjae <jk47wee@gmail.com>
2023-09-28 23:33:56 +0900

    Collections 연습 중

--- 이것이_자바다/this_is_java/src/GDSC/hw01/Test03/Main.java
+++
@@ -1,9 +0,0 @@
-package GDSC.hw01.Test03;
-
-public class Main {
-    public static void main(String[] args) {
-        Student student = new Student("오동재", 25, "F");
-
-        student.introduce();
-    }
-}
