2648547da4bae236e4b9536d40298b580e3923bf
djdongjae <jk47wee@gmail.com>
2023-10-02 05:12:07 +0900

    알고리즘 과제 hw_1 병합정렬 완료

--- 알고리즘/algorithm/src/hw4_1/Main_201914089.java
+++ 알고리즘/algorithm/src/hw4_1/Main_201914089.java
@@ -7,29 +7,46 @@ public class Main_201914089 {
     public static void main(String[] args) {
 
         System.out.println("hw4_1:오동재");
-
         Scanner sc = new Scanner(System.in);
+
+        // 입력받을 개수인 n 입력
         int n = sc.nextInt();
+        // 크기 n의 배열을 생성
         int[] array = new int[n];
+        // n개의 정수 입력받아 배열에 저장
         for (int i = 0; i < n; i++) {
             array[i] = sc.nextInt();
         }
 
-        
-
+        // 병합정렬 수행
+        mergeSort(array, 0, array.length - 1);
+        // 출력
+        for (int i = 0; i < n; i++) {
+            System.out.print(array[i] + " ");
+        }
 
     }
 
-// 정렬된 두 부분 배열 array[lb...m], array[m+1...ub]를 병합하여 정렬된 배열 array[lb...ub]를 얻음
+    // 병합 정렬 함수
+    public static void mergeSort(int[] array, int lb, int ub) {
+        if (lb < ub) { // 시작점이 끝점보다 작을 경우
+            int m = (lb + ub) / 2; // 중간값 지정
+            mergeSort(array, lb, m); // 전반부 정렬
+            mergeSort(array, m+1, ub); // 후반부 정렬
+            merge(array, lb, m, ub); // 병합
+        }
+    }
 
+
+    // 정렬된 두 부분 배열 array[lb...m], array[m+1...ub]를 병합하여 정렬된 배열 array[lb...ub]를 얻음
     public static void merge(int[] array, int lb, int m, int ub) {
-        int[] tmp = new int[ub - lb + 1]; // 결과를 임시 저장할 tmp 배열 생성
+        int[] tmp = new int[array.length]; // 결과를 임시 저장할 tmp 배열 생성
         int left = lb; // 왼쪽 배열의 시작점
         int right = m + 1; // 오른쪽 배열의 시작점
         int index = left; // tmp 배열의 시작점
 
         while (left <= m && right <=ub) { // 왼쪽 배열과 오른쪽 배열의 인덱스가 범위를 벗어나지 않는 동안
-            if ((array[left] % 10) < (array[right] % 10)) { // 왼쪽 값과 오른쪽 값을 비교하며
+            if ((array[left] % 10) <= (array[right] % 10)) { // 왼쪽 값과 오른쪽 값을 비교하며
                 tmp[index] = array[left]; // 왼쪽이 작을 경우 먼저 저장
                 left++; // 왼쪽 인덱스 1 증가
             } else {
