dd3aac1aa57b71ed0fbf5795c80504285642bb8e
djdongjae <jk47wee@gmail.com>
2023-10-01 15:55:10 +0900

    .

---
+++ 이것이_코딩테스트다/this_is_codingtest/src/ch12/prob07/Main.java
@@ -0,0 +1,32 @@
+package ch12.prob07;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String score = sc.next();
+        int length = score.length() / 2;
+        String left = score.substring(0, length);
+        String right = score.substring(length);
+
+        int leftSum = 0;
+        int rightSum = 0;
+
+        for (int i = 0; i < left.length(); i++) {
+            int num = left.charAt(i) - '0';
+            leftSum += num;
+        }
+
+        for (int i = 0; i < right.length(); i++) {
+            int num = right.charAt(i) - '0';
+            rightSum += num;
+        }
+
+        if (leftSum == rightSum) {
+            System.out.println("LUCKY");
+        } else {
+            System.out.println("READY");
+        }
+    }
+}
