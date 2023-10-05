954d9b7a72d831a78b12caadfea44afceef62ab0
djdongjae <jk47wee@gmail.com>
2023-10-05 23:08:53 +0900

    chap12 구현문제 323p 문자열 압축

--- 이것이_코딩테스트다/this_is_codingtest/src/ch12/prob09/Solution.java
+++ 이것이_코딩테스트다/this_is_codingtest/src/ch12/prob09/Solution.java
@@ -1,7 +1,28 @@
 package ch12.prob09;
 
+// chap12 구현문제 323p 문자열 압축
 public class Solution {
     public static int solution(String s) {
-        
+        int answer = s.length();
+        for (int step = 1; step < s.length() / 2 + 1; step++) {
+            String compressed = "";
+            String prev = s.substring(0, step);
+            int cnt = 1;
+            for (int j = step; j < s.length(); j += step) {
+                String sub = "";
+                for (int k = j; k < j + step; k++) {
+                    if (k < s.length()) sub += s.charAt(k);
+                }
+                if (prev.equals(sub)) cnt++;
+                else {
+                    compressed += (cnt >= 2) ? cnt + prev : prev;
+                    prev = sub;
+                    cnt = 1;
+                }
+            }
+            compressed += (cnt >= 2) ? cnt + prev : prev;
+            answer = Math.min(answer, compressed.length());
+        }
+        return answer;
     }
 }
