4b1754e8fc66d9f9f3c02be54bae1692c3c035e4
djdongjae <jk47wee@gmail.com>
2023-10-19 16:27:49 +0900

    TreeSet, Comparable, Comparator, Stack

---
+++ 이것이_자바다/this_is_java/src/ch15_2/sec06/exam01/Coin.java
@@ -0,0 +1,13 @@
+package ch15_2.sec06.exam01;
+
+public class Coin {
+    private int value;
+
+    public Coin(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
