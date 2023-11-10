2d9f147e73c53e719510720c7a7d1018b1e35a61
djdongjae <jk47wee@gmail.com>
2023-11-10 23:42:13 +0900

    알고리즘 lab9_1

---
+++ 알고리즘/algorithm/src/lab9_1/Main_201914089.java
@@ -0,0 +1,52 @@
+package lab9_1;
+
+import java.util.Scanner;
+
+
+
+public class Main_201914089 {
+
+    public static void main(String[] args) {
+
+        System.out.println("lab9_1:오동재");
+        Scanner scanner = new Scanner(System.in);
+
+        // 동전 종류 개수(n)와 n가지 동전 액면을 입력
+        int n = scanner.nextInt();// 동전 종류 개수
+        int[] coins = new int[n];       // 동전 액면
+        for(int i = 0; i < n; i++) {
+            coins[i] = scanner.nextInt();
+        }
+
+        // 원하는 금액 amount를 입력
+        int amount = scanner.nextInt();
+        scanner.close();
+
+        // 최소 동전 개수를 구함
+        int[] dp = new int[amount + 1];  // dp[i]는 금액 i를 만들기 위해 필요한 최소 동전수
+        int[] use = new int[amount + 1]; // use[i]는 금액 i를 만들 때 어떤 액면의 동전이 마지막으로 사용되었는지를 나타냄
+
+        for (int i = 1; i <= amount; i++) {
+            int min = amount + 1;
+            dp[i] = min;   // 금액 i를 만들기 위해 필요한 최소 동전수
+            use[i] = minCoin; // 금액 i를 만들 때 마지막으로 사용된 동전 액면
+        }
+
+
+
+        if (dp[amount] == amount + 1) {  // 나올 수 없는 큰 값이 그대로 저장되어 있으면
+            System.out.println("해가 없습니다.");
+        } else {
+            System.out.println("최소 동전수 = " + dp[amount]);
+        }
+
+
+
+        // 확인을 위해 자료구조 출력
+        for(int i = 0; i < dp.length; i++)
+            System.out.print(dp[i] + " ");
+        System.out.println();
+        for(int i = 0; i < use.length; i++)
+            System.out.print(use[i] + " ");
+    }
+}
