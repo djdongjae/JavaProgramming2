e15c14e8f589e4b6ee3bfc65df9045076eabdf9a
djdongjae <jk47wee@gmail.com>
2024-03-15 17:48:51 +0900

    BOJ_10816 풀이중

--- 이것이_코딩테스트다/this_is_codingtest/src/ch13/prob15/Main.java
+++ 이것이_코딩테스트다/this_is_codingtest/src/ch13/prob15/Main.java
@@ -1,2 +1,21 @@
-package ch13.prob15;public class Main {
+package ch13.prob15;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static int n, m, k, x;
+    public static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+    public static int[] distance = new int[300001];
+    public static int[] visited = new int[300001];
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        
+
+    }
+
 }
